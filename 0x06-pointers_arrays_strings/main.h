23871845f5560871bb1006eab8cff52bdac4688e
root <abeneman123@gmail.com>
2022-09-22 12:05:32 -0700

    a

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -2,12 +2,12 @@
 #define MAIN_H
 #include <stdio,h>
 
+char *_strcat(char *dest, char *src);
 void reverse_array(int *a, int n);
 void print_number(int n);
 void print_buffer(char *b, int size);
 int _strcmp(char *s1, char *s2);
 int _putchar(char c);
-char *_strcat(char *dest, char *src);
 char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char *string_toupper(char *);

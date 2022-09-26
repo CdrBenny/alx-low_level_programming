0a033620961a2b0b318c19ffd002dee3689b72c5
root <abeneman123@gmail.com>
2022-09-26 08:36:01 -0700

    as

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,13 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/**
- * File: main.h
- * description: containing all prototypes
- */
-
-#include <stddef.h>
-
 int _putchar(char);
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);

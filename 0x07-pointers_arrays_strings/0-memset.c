f842081dc09eeccaf165417e764a4a3f7ade228e
root <abeneman123@gmail.com>
2022-09-26 08:15:38 -0700

    abene

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <string.h>
 
 /**
  * _memset -> this memory set function
@@ -9,5 +10,10 @@
  */
 char *_memset(char *s, char b, unsigned int n)
 {
-	return (memset(s,b,n);
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		s[a] = b;
+
+	return (s);
 }

64fc63030bd96524666e8bf9bb771f4d9c4484d8
root <abeneman123@gmail.com>
2022-09-26 07:38:31 -0700

    @

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -6,18 +6,16 @@
  * @s: a pointer
  * @c: the character
  * @n: the number
- *  description _memset: over there
- *
  *  Return: A pointer to the filled memory @s.
  */
 
-void *_memset(void *s, int c, size_t n)
+char *_memset(char *s, char b, unsigned int n)
 {
-	unsigned int index;
-	unsigned char *memory = s, value = c;
-
-	for (index = 0; index < n; index++)
-		memory[index] = value;
-
-	return (memory);
+	while (n)
+	{
+		s[n - 1] = b;
+		n--;
+	}
+	return (s);
 }
+		

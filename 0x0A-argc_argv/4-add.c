7237bf32a90828582fa98036aa274f2913ca23eb
root <abeneman123@gmail.com>
2022-09-30 13:06:48 -0700

    a

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -2,31 +2,6 @@
 #include <stdlib.h>
 #include <ctype.h>
 #include <string.h>
-
-/**
- * check_num - check - string there are digit
- * @str: array str
- *
- * Return: Always 0 (Success)
- */
-
-int check_num(char *str)
-{
-	unsigned int count;
-
-	count = 0;
-	while (count < strlen(str))
-	{
-		if (!isdigit(sit[count]))
-		{
-			return (0);
-		}
-
-		count ++;
-	}
-	return (1);
-}
-
 /**
  * main - Print the name of the program
  * @argc: Count arguments

128a54f44c799fa28106f4b0298150c1f51b9e8e
root <abeneman123@gmail.com>
2022-09-30 11:21:11 -0700

    a

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,14 +8,14 @@
  * @argv: vector
  * Return: 0 true, 1 false
  */
-int main(int agrc, char *argv[])
+int main(int argc, char *argv[])
 {
 	int a, b;
 	if (argc == 3)
 	{
 		a = atoi(argv[1]);
 		b = atoi(argv[2]);
-		printf("%d\n", a *b);
+		printf("%d\n", a * b);
 		return (0);
 	}
 	printf("Error\n");

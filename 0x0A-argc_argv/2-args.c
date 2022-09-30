6ad145becb7f61fa71a958146f92d780cd02186f
root <abeneman123@gmail.com>
2022-09-30 10:38:04 -0700

    a

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -7,12 +7,11 @@
  * @argv: Arguments
  * Return: Always 0(success)
  */
-
 int main(int argc, char *argv[])
 {
 	int count = 0; /* Derclaring bariables*/
 
-	ig (argc > 0)
+	if (argc > 0)
 	{
 		while (count < argc)
 		{

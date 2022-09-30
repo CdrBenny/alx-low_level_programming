95627efb3e01a96a2fda41b3d84aa707bf931708
root <abeneman123@gmail.com>
2022-09-30 12:39:16 -0700

    Z

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -6,18 +6,22 @@
  * main - prints the multiplication
  * @argc: Argument
  * @argv: vector
- * Return: 0 true, 1 false
+ * Return: 0 if true, 1 if false
  */
 int main(int argc, char *argv[])
 {
-	int a, b;
-	if (argc == 3)
+	int index, multiplication;
+
+	multiplication = 1;
+	if (argc < 3)
 	{
-		a = atoi(argv[1]);
-		b = atoi(argv[2]);
-		printf("%d\n", a * b);
-		return (0);
+		printf("Error\n");
+		return (1);
 	}
-	printf("Error\n");
-	return (1);
+	for (index = 1; index < argc; index++)
+	{
+		multiplication *= atoi(argv[index]);
+	}
+	printf("%d\n", multiplication);
+	return (0);
 }

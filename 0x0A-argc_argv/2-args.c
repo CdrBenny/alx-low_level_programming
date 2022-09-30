cc2e3a07f168ca3ef57df741a413151e7e7eb5fb
root <abeneman123@gmail.com>
2022-09-30 10:34:55 -0700

    a

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Print the name of the program
+ * @argc: Count Arguments
+ * @argv: Arguments
+ * Return: Always 0(success)
+ */
+
+int main(int argc, char *argv[])
+{
+	int count = 0; /* Derclaring bariables*/
+
+	ig (argc > 0)
+	{
+		while (count < argc)
+		{
+			printf("%s\n", argc[count]);
+
+			return (0);
+		}
+	}
+}
+

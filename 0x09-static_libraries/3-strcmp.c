c43af578ac513796829d98831b5b0a5420d5bc9f
root <abeneman123@gmail.com>
2022-10-01 02:25:47 -0700

    a

---
+++ 0x09-static_libraries/3-strcmp.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *  _strcmp - function which comare two strings and
+ *  @s1: first string
+ *  @s2: second string
+ *
+ *  Return:
+ *                returns zero if s1 == s2
+ *                returns negative number if s1 < s2
+ *                returns positive number if s1 > s2
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2 && *s1 == *s2)
+	{
+		s1++;
+		s2++;
+	}
+
+	return (*s1 - *s2);
+}

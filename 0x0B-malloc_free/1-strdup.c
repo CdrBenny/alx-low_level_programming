0814d01319c9982ec1c041a3b667c620ebcbe2c1
root <abeneman123@gmail.com>
2022-10-04 09:14:54 -0700

    #

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -21,13 +21,13 @@ char *_strdup(char *str)
 		i++;
 	s = (char *) malloc(i * sizeof(char) + 1);
 	if (s == NULL)
-		return(NULL);
+		return (NULL);
 	while (j < i)
 	{
 		s[j] = str[j];
 		j++;
 	}
 
-	s[j] ='\0';
+	s[j] = '\0';
 	return (s);
 }

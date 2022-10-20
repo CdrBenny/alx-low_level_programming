c8a86e9ea76d4bdc531360ccaabebd5859ef4efd
Abenu <abeneman123@gmail.com>
2022-10-20 10:01:10 -0700

    Benny

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -6,6 +6,19 @@
  */
 
 #include <stdlib.h>
+/**
+ * struct list_s - singly
+ * @str: string - (malloc'ed string)
+ * @len: length
+ * @next: points
+ */
+typedef struct list_s
+{
+	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
+
 
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
